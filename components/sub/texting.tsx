2486bc122c72f6ef897021a9ec9764015a0edec3
OKERE PROMISE CHINONSO <111078201+Rexisle@users.noreply.github.com>
2026-03-03 19:15:51 +0100

    Change background color class to indigo-600

--- components/sub/texting.tsx
+++ components/sub/texting.tsx
@@ -2,7 +2,7 @@ import { Typewriter } from "react-simple-typewriter";
 
 export default function Texting() {
   return (
-    <h1 className="text-4xl text-center text-transparent bg-clip-text border-indigo-600 font-mono">
+    <h1 className="text-4xl text-center text-transparent bg-clip-text bg-indigo-600 font-mono">
       &nbsp;
       <Typewriter
       words={["Web Developer", "Problem Solver", "Tech Enthusiast"]}
