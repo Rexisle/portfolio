20ebf75ab9c921e3e40d8abcca5cdc761385a4b8
OKERE PROMISE CHINONSO <111078201+Rexisle@users.noreply.github.com>
2026-03-03 19:01:18 +0100

    Update Texting component header styling

--- components/sub/texting.tsx
+++ components/sub/texting.tsx
@@ -2,7 +2,7 @@ import { Typewriter } from "react-simple-typewriter";
 
 export default function Texting() {
   return (
-    <h1 className="text-4xl text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 font-mono">
+    <h1 className="text-4xl text-center text-transparent bg-clip-text border-indigo-600 font-mono">
       &nbsp;
       <Typewriter
       words={["Web Developer", "Problem Solver", "Tech Enthusiast"]}
